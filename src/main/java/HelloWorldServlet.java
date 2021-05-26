e6a170072e96d6825cd6b9ba6f24452666d914a2
Salim Ahmedabadi <ahmedabadi.salim@gmail.com>
2021-05-26 11:48:00 -0500

    feat: Started working on Servlet Exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,24 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
+@WebServlet(name = "MarcoHelloWorld", urlPatterns = "/Hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+
+        String name = request.getParameter("name");
+
+        if(name == null){
+            name = "Hello World!";
+
+            response.getWriter().println("<h1>Hello World!</h1>");
+            response.getWriter().println("<h1>Hello Codeup!</h1>");
+        }
+
 
     }
 }
