359aa18fd4c6991d5353f5c7ec9df6425e03cf7e
Salim Ahmedabadi <ahmedabadi.salim@gmail.com>
2021-05-27 14:03:30 -0500

    feat: Modified Servlets Exercise file

--- src/main/java/count.java
+++ src/main/java/count.java
@@ -4,7 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "MarcoCount", urlPatterns = "/Count")
+@WebServlet(name = "MarcoCount", urlPatterns = "/count")
 public class count extends HttpServlet {
     int count = 0;
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
