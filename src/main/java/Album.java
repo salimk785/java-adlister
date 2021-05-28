1855314fea96a12d26d939d2eaa83f359c2f89e7
Salim Ahmedabadi <ahmedabadi.salim@gmail.com>
2021-05-28 17:35:21 -0500

    feat: Modified Result.jsp file

---
+++ src/main/java/Album.java
@@ -0,0 +1,13 @@
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Album implements Serializable{
+    private long id;
+    private String artist;
+    private String lastName;
+    private Date releaseDate;
+
+
+
+}
---
+++ src/main/webapp/result.jsp
@@ -0,0 +1,15 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+
+<head>
+    <title>Home</title>
+
+</head>
+</html>
+
+<body>
+
+
+
+</body>
+</html>
